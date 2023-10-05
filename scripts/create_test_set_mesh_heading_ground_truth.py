ab3621c60013ef8a97595ff627fb0d10de1f7ea3
raear <raear@nih.gov>
2023-10-05 17:51:13 -0400

    Add create mesh heading ground truth script

--- scripts/create_test_set_mesh_heading_ground_truth.py
+++ scripts/create_test_set_mesh_heading_ground_truth.py
@@ -1,10 +1,11 @@
+import gzip
 import json
 import os.path
 import pandas as pd
 import pytrec_eval
 
 
-WORKING_DIR="/net/intdev/pubmed_mti/ncbi/working_dir/mtix/scripts/create_test_set_descriptor_ground_truth"
+WORKING_DIR="/net/intdev/pubmed_mti/ncbi/working_dir/mtix/scripts_v3/create_test_set_mesh_heading_ground_truth"
 
 
 def create_lookup(path):
@@ -14,16 +15,20 @@ def create_lookup(path):
 
 
 def main():
-    desc_names_path =    os.path.join(WORKING_DIR, "main_heading_names.tsv")
-    desc_uis_path =      os.path.join(WORKING_DIR, "main_headings.tsv")
-    qrels_path =         os.path.join(WORKING_DIR, "test_set_2017-2023_qrels.tsv")
-    ground_truth_path =  os.path.join(WORKING_DIR, "test_set_2017-2023_Descriptor_Ground_Truth.json")
-    test_set_data_path = os.path.join(WORKING_DIR, "test_set_data.json")
+    ground_truth_path =  os.path.join(WORKING_DIR, "val_set_2017-2023_MeSH_Heading_Ground_Truth.json")
+    names_path =         os.path.join(WORKING_DIR, "mesh_heading_names_2023.tsv")
+    qrels_path =         os.path.join(WORKING_DIR, "val_set_2017-2023_qrels.tsv")
+    test_set_data_path = os.path.join(WORKING_DIR, "val_set_2017-2023_data.json.gz")
+    types_path =         os.path.join(WORKING_DIR, "mesh_heading_types_2023.tsv")
+    uis_path =           os.path.join(WORKING_DIR, "mesh_heading_uis_2023.tsv")
 
-    desc_names = create_lookup(desc_names_path)
-    desc_uis =   create_lookup(desc_uis_path)
+    names = create_lookup(names_path)
+    types = create_lookup(types_path)
+    uis =   create_lookup(uis_path)
+    
     qrels = pytrec_eval.parse_qrel(open(qrels_path))
-    test_set_data = json.load(open(test_set_data_path))
+    test_set_data = json.load(gzip.open(test_set_data_path, mode="rt", encoding="utf8"))
+    
     data_lookup = { citation_data["uid"]: citation_data["data"] for citation_data in test_set_data}
 
     mti_json = []
@@ -33,12 +38,13 @@ def main():
         mti_json.append(citation_predictions)
         for p_id in qrels[q_id]: 
             label_id = int(p_id)
-            name = desc_names[label_id]
-            ui =   desc_uis[label_id]
+            name = names[label_id]
+            _type = types[label_id]
+            ui =   uis[label_id]
             score = 1.
             citation_predictions["Indexing"].append({
                 "Term": name, 
-                "Type": "Descriptor", 
+                "Type": _type, 
                 "ID": ui, 
                 "IM": "NO", 
                 "Reason": f"score: {score:.3f}"})
